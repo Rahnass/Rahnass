72d2288895f3b5eb188b17b8a49f103f20852806
Rahnass <rahanassv@gmail.com>
2022-08-29 10:20:12 -0700

    sixth commit

--- TallyApp/models.py
+++ TallyApp/models.py
@@ -51,7 +51,7 @@ class Ledger(models.Model):
         return self.ledger_name
 
 class L_Tax_Register(models.Model):
-    ledger_id = models.ForeignKey(Ledger, on_delete=models.CASCADE, null=True, blank=True)
+    ledger_id = models.ForeignKey(Ledger, on_delete=models.CASCADE, null=True)
     gst_uin = models.CharField(max_length=225,default="Null",blank=True)
     register_type =models.CharField(max_length=225,default="Null",blank=True)
     pan_no = models.CharField(max_length=225,default="Null",blank=True)
@@ -62,7 +62,7 @@ class L_Tax_Register(models.Model):
 
 
 class Ledger_Satutory(models.Model):
-    ledger_id = models.ForeignKey(Ledger, on_delete=models.CASCADE, null=True, blank=True)
+    ledger_id = models.ForeignKey(Ledger, on_delete=models.CASCADE, null=True)
     assessable_calculation = models.CharField(max_length=225,default="Null",blank=True)
     Appropriate_to =models.CharField(max_length=225,default="Null",blank=True)
     gst_applicable = models.CharField(max_length=225,default="Null",blank=True)
@@ -75,12 +75,12 @@ class Ledger_Satutory(models.Model):
 
 
 class Ledger_Rounding(models.Model):
-    ledger_id = models.ForeignKey(Ledger, on_delete=models.CASCADE, null=True, blank=True)
+    ledger_id = models.ForeignKey(Ledger, on_delete=models.CASCADE, null=True)
     Rounding_Method =models.CharField(max_length=225,default="Null",blank=True)
     Round_limit = models.CharField(max_length=22,default="Null",blank=True)
 
 class Ledger_tax(models.Model):
-    ledger_id = models.ForeignKey(Ledger, on_delete=models.CASCADE, null=True, blank=True)
+    ledger_id = models.ForeignKey(Ledger, on_delete=models.CASCADE, null=True)
     type_of_duty_or_tax =models.CharField(max_length=225,default="Null",blank=True)
     type_of_tax =models.CharField(max_length=225,default="Null",blank=True)
     valuation_type=models.CharField(max_length=225,default="Null",blank=True)
@@ -89,7 +89,7 @@ class Ledger_tax(models.Model):
    
 
 class Ledger_sundry(models.Model):
-    ledger_id = models.ForeignKey(Ledger, on_delete=models.CASCADE, null=True, blank=True)
+    ledger_id = models.ForeignKey(Ledger, on_delete=models.CASCADE, null=True)
     maintain_balance_bill_by_bill =models.CharField(max_length=225,default="Null",blank=True)
     Default_credit_period=models.CharField(max_length=225,default="Null",blank=True)
     Check_for_credit_days=models.CharField(max_length=225,default="Null",blank=True)
@@ -97,7 +97,7 @@ class Ledger_sundry(models.Model):
 
 
 class Ledger_Banking_Details(models.Model):
-    ledger_id = models.ForeignKey(Ledger, on_delete=models.CASCADE, null=True, blank=True)
+    ledger_id = models.ForeignKey(Ledger, on_delete=models.CASCADE, null=True)
     od_limit = models.CharField(max_length=225,default="Null",blank=True)
     holder_name =models.CharField(max_length=225,default="Null",blank=True)
     ac_number =models.CharField(max_length=225,default="Null",blank=True)
@@ -115,13 +115,7 @@ class Ledger_Banking_Details(models.Model):
 
 
 class Ledger_Mailing_Address(models.Model):
-    ledger_id = models.ForeignKey(Ledger, on_delete=models.CASCADE, null=True, blank=True)
-    ledger_bank = models.ForeignKey(Ledger_Banking_Details, on_delete=models.CASCADE, null=True, blank=True)
-    ledger_tax = models.ForeignKey(L_Tax_Register, on_delete=models.CASCADE, null=True, blank=True)
-    ledger_sat = models.ForeignKey(Ledger_Satutory,  on_delete=models.CASCADE,  null=True, blank=True)
-    ledger_round = models.ForeignKey(Ledger_Rounding, on_delete=models.CASCADE, null=True, blank=True)
-    l_tax = models.ForeignKey(Ledger_tax, on_delete=models.CASCADE, null=True, blank=True)
-    l_sundry = models.ForeignKey(Ledger_sundry, on_delete=models.CASCADE, null=True, blank=True)
+    ledger_id = models.ForeignKey(Ledger, on_delete=models.CASCADE, null=True)
     name = models.CharField(max_length=225,default="Null",blank=True)
     address = models.CharField(max_length=225,default="Null",blank=True)
     state = models.CharField(max_length=225,default="Null",blank=True)
--- TallyApp/views.py
+++ TallyApp/views.py
@@ -45,8 +45,9 @@ def ledgers(request):
 
 def ledger_alter(request,pk):
     grpp=Ledger.objects.get(id=pk)
-    gr=Ledger_Mailing_Address.objects.filter(ledger_id=grpp)
-    context={'gr':gr}
+    gr=Ledger_Mailing_Address.objects.filter(ledger_id_id=grpp)
+    bnk=Ledger_Banking_Details.objects.filter(ledger_id_id=grpp)
+    context={'a':grpp,'gr':gr,'bnk':bnk}
     return render(request, 'ledger_alter.html',context)  
 
 
@@ -62,68 +63,92 @@ def alter_ledger(request,pk):
 
         led.save()
         idd = led.id
-
-        ledm = Ledger_Mailing_Address.objects.get(id=pk)
-        ledm.name = request.POST.get('name', False)
-        ledm.address = request.POST.get('address', False)
-        ledm.state = request.POST.get('state', False)
-        ledm.country = request.POST.get('country', False)
-        ledm.pincode = request.POST.get('pincode', False)
-
-        ledm.save()
-
-        ledb = Ledger_Banking_Details.objects.get(id=idd)
-        ledb.od_limit = request.POST.get('od_limit', False)
-        ledb.holder_name =request.POST.get('holder_name', False)
-        ledb.ac_number = request.POST.get('ac_number', False)
-        ledb.ifsc = request.POST.get('ifsc', False)
-        ledb.swift_code =request.POST.get('swift_code', False)
-        ledb.bank_name = request.POST.get('bank_name', False)
-        ledb.branch_name = request.POST.get('branch_name', False)
-        ledb.alter_chk_bks =request.POST.get('alter_chk_bks', False)
-        ledb.enbl_chk_printing = request.POST.get('enbl_chk_printing', False) 
-
-        ledb.save()
-
-        ledt = L_Tax_Register.objects.get(id=idd)
-        ledt.gst_uin = request.POST.get('gst_uin', False)
-        ledt.register_type = request.POST.get('register_type', False)
-        ledt.pan_no = request.POST.get('pan_no', False)
-        ledt.alter_gst_details =request.POST.get('alter_gst_details', False)
-
-        ledt.save()
-
-        leds = Ledger_Satutory.objects.get(id=idd)
-        leds.assessable_calculation = request.POST.get('assessable_calculation', False)
-        leds.Appropriate_to =request.POST.get('Appropriate_to', False)
-        leds.gst_applicable = request.POST.get('is_gst_applicable',False)
-        leds.Set_alter_GST =request.POST.get('Set_alter_GST', False)
-        leds.type_of_supply = request.POST.get('type_of_supply',False)
-        leds.Method_of_calc =request.POST.get('Method_of_calc', False)
-
-        leds.save()
-
-        ledr = Ledger_Rounding.objects.get(id=idd)
-        ledr.Rounding_Method = request.POST.get('Rounding_Method', False)
-        ledr.Round_limit  = request.POST.get('Round_limit', False)
-
-        ledr.save()
         
-        ledtax = Ledger_tax.objects.get(id=idd)
-        ledtax.type_of_duty_or_tax =request.POST.get('type_of_duty_or_tax', False)
-        ledtax.type_of_tax =request.POST.get('type_of_tax', False)
-        ledtax.valuation_type =request.POST.get('valuation_type', False)
-        ledtax.rate_per_unit =request.POST.get('rate_per_unit', False)
-        ledtax.Persentage_of_calculation =request.POST.get('Persentage_of_calculation', False)
+        try:
+            ledm = Ledger_Mailing_Address.objects.get(id=pk)
+            ledm.name = request.POST.get('name', False)
+            ledm.address = request.POST.get('address', False)
+            ledm.state = request.POST.get('state', False)
+            ledm.country = request.POST.get('country', False)
+            ledm.pincode = request.POST.get('pincode', False)
 
-        ledtax.save()
+            ledm.save()
+        except Ledger_Mailing_Address.DoesNotExist:
+            ledm = None
+            
+        try:
+            ledb = Ledger_Banking_Details.objects.get(id=idd).first()
+            ledb.od_limit = request.POST.get('od_limit', False)
+            ledb.holder_name =request.POST.get('holder_name', False)
+            ledb.ac_number = request.POST.get('ac_number', False)
+            ledb.ifsc = request.POST.get('ifsc', False)
+            ledb.swift_code =request.POST.get('swift_code', False)
+            ledb.bank_name = request.POST.get('bank_name', False)
+            ledb.branch_name = request.POST.get('branch_name', False)
+            ledb.alter_chk_bks =request.POST.get('alter_chk_bks', False)
+            ledb.enbl_chk_printing = request.POST.get('enbl_chk_printing', False) 
+            ledb.save()
+        except Ledger_Banking_Details.DoesNotExist:
+            ledb = None
 
-        ledsun = Ledger_sundry.objects.get(id=idd)
-        ledsun.maintain_balance_bill_by_bill =request.POST.get('maintain_balance_bill_by_bill', False)
-        ledsun.Default_credit_period =request.POST.get('Default_credit_period', False)
-        ledsun.Check_for_credit_days =request.POST.get('Check_for_credit_days', False)    
+        
+        try:
+            ledt = L_Tax_Register.objects.get(id=idd)
+            ledt.gst_uin = request.POST.get('gst_uin', False)
+            ledt.register_type = request.POST.get('register_type', False)
+            ledt.pan_no = request.POST.get('pan_no', False)
+            ledt.alter_gst_details =request.POST.get('alter_gst_details', False)
 
-        ledsun.save() 
+            ledt.save()
+        except L_Tax_Register.DoesNotExist:
+            ledt = None
+
+        try:
+            leds = Ledger_Satutory.objects.get(id=idd)
+            leds.assessable_calculation = request.POST.get('assessable_calculation', False)
+            leds.Appropriate_to =request.POST.get('Appropriate_to', False)
+            leds.gst_applicable = request.POST.get('is_gst_applicable',False)
+            leds.Set_alter_GST =request.POST.get('Set_alter_GST', False)
+            leds.type_of_supply = request.POST.get('type_of_supply',False)
+            leds.Method_of_calc =request.POST.get('Method_of_calc', False)
+
+            leds.save()
+        except Ledger_Satutory.DoesNotExist:
+            leds = None   
+
+        try:
+
+            ledr = Ledger_Rounding.objects.get(id=idd)
+            ledr.Rounding_Method = request.POST.get('Rounding_Method', False)
+            ledr.Round_limit  = request.POST.get('Round_limit', False)
+
+            ledr.save()
+        except Ledger_Rounding.DoesNotExist:
+            ledr = None
+
+        try:
+
+            ledtax = Ledger_tax.objects.get(id=idd)
+            ledtax.type_of_duty_or_tax =request.POST.get('type_of_duty_or_tax', False)
+            ledtax.type_of_tax =request.POST.get('type_of_tax', False)
+            ledtax.valuation_type =request.POST.get('valuation_type', False)
+            ledtax.rate_per_unit =request.POST.get('rate_per_unit', False)
+            ledtax.Persentage_of_calculation =request.POST.get('Persentage_of_calculation', False)
+
+            ledtax.save()
+        except Ledger_tax.DoesNotExist:
+            ledtax = None
+
+        try:
+
+            ledsun = Ledger_sundry.objects.get(id=idd)
+            ledsun.maintain_balance_bill_by_bill =request.POST.get('maintain_balance_bill_by_bill', False)
+            ledsun.Default_credit_period =request.POST.get('Default_credit_period', False)
+            ledsun.Check_for_credit_days =request.POST.get('Check_for_credit_days', False)    
+
+            ledsun.save() 
+        except Ledger_sundry.DoesNotExist:
+            ledsun = None    
 
         return redirect('ledgers')
     return render(request,'ledger_alter.html')
